eb9dd95a0e68d80d2638fe02ffa452adfb535a5e
Numa Jequier <numa.jequier@gmail.com>
2017-11-07 11:41:34 +0100

    add routing schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'tasks' => "tasks#index"
+  get 'tasks/new' => "tasks#new"
+  get 'tasks/:id' => "tasks#show"
+  post 'tasks' => "tasks#create"
+  get 'tasks/:id/edit' => "tasks#edit"
+  patch 'tasks/:id' => "tasks#update"
+  delete 'tasks/:id' => "tasks#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
